b9ccc568fb9d8503d1a772223d9df3449e9ab5e8
Bartosz Zbytniewki <zbyti@yahoo.com>
2022-04-06 11:28:00 +0200

    Mad Pascal / Snake - VIC-20 PAL

---
+++ Mad Pascal/snake/src/vic20.pas
@@ -0,0 +1,40 @@
+{$librarypath 'src/systems'}
+
+uses sys_vic20;
+
+const
+  GAME_BLACK  = BLACK;
+  GAME_WHITE  = WHITE;
+  GAME_RED    = RED;
+  GAME_CYAN   = CYAN;
+  GAME_PURPLE = PURPLE;
+  GAME_GREEN  = GREEN;
+  GAME_BLUE   = BLUE;
+  GAME_YELLOW = YELLOW;
+  GAME_ORANGE = ORANGE;
+
+  C_FRUIT      = 0;
+  C_SPACE      = $20;
+
+  C_WALL_H     = $1e;
+  C_WALL_V     = $1f;
+
+  C_HEAD_UP    = $27;
+  C_HEAD_RIGHT = $26;
+  C_HEAD_DOWN  = $28;
+  C_HEAD_LEFT  = $29;
+
+  C_TAIL_UP    = $22;
+  C_TAIL_LEFT  = $23;
+  C_TAIL_DOWN  = $24;
+  C_TAIL_RIGHT = $25;
+
+  C_BODY_V     = $2a;
+  C_BODY_H     = $2b;
+
+  C_BODY_SW    = $3b;
+  C_BODY_SE    = $3c;
+  C_BODY_NW    = $3d;
+  C_BODY_NE    = $3e;
+
+{$i 'src/game.inc'}
